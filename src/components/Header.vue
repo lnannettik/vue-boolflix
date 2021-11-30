86f07c4519b0078e59eb73cfd60b730c9155b9e8
lnannettik <l.nannetti.k@gmail.com>
2021-11-30 17:24:17 +0100

    start

---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+    <header>
+        <input type="text" placeholder="Search a movie" v-model="searchText" />
+
+        <button>Search</button>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+    data() {
+        return {
+            searchText: '',
+        };
+    },
+};
+
+</script>
+
+<style>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import 'bootstrap/scss/bootstrap.scss';
+
+
+
 Vue.config.productionTip = false
 
 new Vue({
